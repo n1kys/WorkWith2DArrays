8e8edd45f1b00767d6a1a0629ef0c3248e72f933
n1kys <lebednikita12@gmail.com>
2023-05-15 06:41:12 +0300

    Create a fieds and constructor for work with 2D array

--- TechProh_Lab4_Lebed/ClassFor2DArrays.cs
+++ TechProh_Lab4_Lebed/ClassFor2DArrays.cs
@@ -8,6 +8,26 @@ namespace TechProh_Lab4_Lebed
 {
     public class ClassFor2DArrays
     {
+        public int[,] matrix { get; set; }
+
+        public double[,] matrixRes { get; set; }
+        
+        public int size { get; set; }
+
+        public ClassFor2DArrays(int size)
+        {
+            this.size = size;
+            matrix = new int[size, size];
+        }
+
+        public ClassFor2DArrays(int[,] matrix)
+        {
+            this.matrix = matrix;
+        }
+
+        public ClassFor2DArrays() { 
+        }
+
 
     }
 }
